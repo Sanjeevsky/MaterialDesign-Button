80e51459577384691177908f9f18314f43275cb6
sanjeevsky <knit16643@gmail.com>
2019-02-14 13:36:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sanjeev/button/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sanjeev.button;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    AppCompatButton raisedButton;
+    AppCompatButton flatButton;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        raisedButton=(AppCompatButton)findViewById(R.id.raised_btn);
+        flatButton=(AppCompatButton)findViewById(R.id.flat_btn);
+        fab=(FloatingActionButton)findViewById(R.id.flat_btn);
+        raisedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Button Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
+        flatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.animate().rotation(360);
+                Toast.makeText(getApplicationContext(),"Button2 Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
